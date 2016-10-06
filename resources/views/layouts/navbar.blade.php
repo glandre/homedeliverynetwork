cbe0bc33320ebf862807908388433a0da00d0e00
Geraldo B. Landre <geraldo.landre@gmail.com>
2016-10-05 20:09:40 -0400

    ref #5
    Signed-off-by: Geraldo B. Landre <geraldo.landre@gmail.com>

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,28 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: geraldo
- * Date: 05/10/16
- * Time: 20:07
- */
+<div class="navbar-custom active">
+    <div class="container">
+        <div id="navigation" class="active">
+            <!-- Navigation Menu-->
+            <ul class="navigation-menu">
+                <li class="active">
+                    <a href="{{ url('/dashboard') }}"><i class="zmdi zmdi-view-dashboard"></i> <span> Dashboard </span> </a>
+                </li>
+                @if(Auth::user()->is_super)
+                    <li class="has-submenu last-elements">
+                        <a href="{{ url('/users') }}">
+                            <i class="zmdi zmdi-collection-item"></i> <span> Users </span>
+                        </a>
+                        <ul class="submenu megamenu">
+                            <li>
+                                <ul>
+                                    <li><a href="{{ url('/users') }}">Manage Users</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+
+            </ul>
+            <!-- End navigation menu  -->
+        </div>
+    </div>
+</div>
